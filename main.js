aa5ccb2666d7584160018d76331845460d1941ad
Malik Villarreal <mvtech03@gmail.com>
2016-09-09 11:40:47 -0500

    90sretro

--- main.js
+++ main.js
@@ -2,31 +2,71 @@ var globalData = {};
 $(document).ready(function() {
 
 
-      // click events
-      $("#sub").click(function(event){
+
+    // click events
+    $("#sub").click(function(event) {
         event.preventDefault();
+
+        // newCall();
+        // var rightCount = 0;
+        // var wrongCount = 0;
+        // for (count; count < $("#resultss").html(globalData.answer; count++) ){
+        //
+        // }
         $("#resultss").html(globalData.answer);
+        // if ( $(".scoreR").text() == 0 && $(".scoreW").text() == 0) {
+        //
+        //     if ($("#answer").html() === globalData.answer) {
+        //         $(".scoreR").text(( parseInt($(".scoreR").text() ) + 1 ));
+        //     } else {
+        //         $(".scoreW").text(( parseInt($(".scoreW").text() ) + 1 ));
+        //     }
+
+        // } else {
+            if ($("#answer").val() === globalData.answer) {
+
+                $(".scoreR").text(( parseInt($(".scoreR").text() ) + 1 ));
+            } else {
+              $(".scoreW").text(( parseInt($(".scoreW").text() ) + 1 ));
+              var buzzer = $('audio')[0];
+              
+
+            }
+        // }
+
+
+    })
+
+    $("#reset").click(function(event) {
+        newCall();
+        event.preventDefault();
+        $("#question").empty();
+        $("#answer").val('');
+        $("#resultss").empty();
+    });
+    newCall();
+
+    function newCall() {
+        var url = "http://jservice.io/api/random";
+        var grabRandomQuestion = $.ajax({
+            url: url,
+            dataType: "json",
+            type: "GET"
+
         })
 
+        grabRandomQuestion.done(setQuestionAndAnswer);
 
+        function setQuestionAndAnswer(data) {
+            console.log(data);
+            globalData.question = data[0].question;
+            globalData.answer = data[0].answer;
 
+            $("#question").html(globalData.question);
 
-  var $randomQuestion;
-  var url = "http://jservice.io/api/random";
-  $.ajax({
-      url: url,
-      dataType: "json",
-      type: "GET"
+            console.log(data[0].question);
+        };
 
-  }).done(function(data){
-
-     globalData.question = data[0].question;
-     globalData.answer = data[0].answer;
-
-        $("#question").html(globalData.question);
-
-          console.log(data[0].question);
-
-})
+    }
 
 })
